8b4447a95a2c337e6875b81c0f73a2cc31a0e156
Sergey Tosunyan <sergey@inconceptlabs.com>
2024-03-07 12:32:42 +0400

    Implement ProvideThemedContent functions

--- library/src/main/java/com/inconceptlabs/designsystem/utils/ComposeUtils.kt
+++ library/src/main/java/com/inconceptlabs/designsystem/utils/ComposeUtils.kt
@@ -1,6 +1,9 @@
 package com.inconceptlabs.designsystem.utils
 
 import android.view.View
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Indication
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.detectVerticalDragGestures
@@ -14,18 +17,47 @@ import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.Dp
 import androidx.fragment.app.Fragment
 import com.inconceptlabs.designsystem.theme.AppTheme
+import com.inconceptlabs.designsystem.theme.NoIndication
 import com.inconceptlabs.designsystem.theme.attributes.Size
+import com.inconceptlabs.designsystem.theme.colors.ColorScheme
 import com.inconceptlabs.designsystem.theme.tokens.strokeWidthThick
 import com.inconceptlabs.designsystem.theme.tokens.strokeWidthThin
+import com.inconceptlabs.designsystem.theme.typography.Barlow
+import com.inconceptlabs.designsystem.theme.typography.Typography
 
 @Suppress("FunctionName")
-fun Fragment.ComposeViewWithTheme(
-    content: @Composable () -> Unit
+fun Fragment.ProvideThemedContent(
+    colorScheme: ColorScheme = ColorScheme.Light,
+    typography: Typography = Barlow,
+    indication: Indication = NoIndication,
+    content: @Composable () -> Unit,
 ): View {
     return ComposeView(requireContext()).apply {
         setContent {
-            AppTheme(content = content)
+            AppTheme(
+                colorScheme = colorScheme,
+                typography = typography,
+                indication = indication,
+                content = content
+            )
         }
+    }
+}
+
+@Suppress("FunctionName")
+fun ComponentActivity.ProvideThemedContent(
+    colorScheme: ColorScheme = ColorScheme.Light,
+    typography: Typography = Barlow,
+    indication: Indication = NoIndication,
+    content: @Composable () -> Unit,
+) {
+    setContent {
+        AppTheme(
+            colorScheme = colorScheme,
+            typography = typography,
+            indication = indication,
+            content = content
+        )
     }
 }
 
