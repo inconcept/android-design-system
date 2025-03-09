2c2c5f508acb5af9da4d6ee4e73341c7f40150c0
Sergey Tosunyan <sergeytosunyan33@gmail.com>
2025-03-09 23:10:02 +0400

    Upgrade Gradle distribution and AGP versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
     id("org.jetbrains.kotlin.android") version "1.9.21" apply false
-    id("com.android.library") version "8.2.2" apply false
+    id("com.android.library") version "8.9.0" apply false
 }
